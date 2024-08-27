d4de15dc8857632f9a1a6c6c08e861976ca140fe
Koshiro Hashimoto (Woven by Toyota <koshiro.hashimoto@woven-planet.global>
2024-08-27 17:00:42 +0900

    complete part3_sec3

--- Debug/kernel/subdir.mk
+++ Debug/kernel/subdir.mk
@@ -8,8 +8,10 @@ C_SRCS += \
 ../kernel/inittsk.c \
 ../kernel/scheduler.c \
 ../kernel/syslib.c \
+../kernel/systimer.c \
 ../kernel/task_manage.c \
-../kernel/task_queue.c 
+../kernel/task_queue.c \
+../kernel/task_sync.c 
 
 S_UPPER_SRCS += \
 ../kernel/dispatch.S 
@@ -20,8 +22,10 @@ OBJS += \
 ./kernel/inittsk.o \
 ./kernel/scheduler.o \
 ./kernel/syslib.o \
+./kernel/systimer.o \
 ./kernel/task_manage.o \
-./kernel/task_queue.o 
+./kernel/task_queue.o \
+./kernel/task_sync.o 
 
 S_UPPER_DEPS += \
 ./kernel/dispatch.d 
@@ -31,8 +35,10 @@ C_DEPS += \
 ./kernel/inittsk.d \
 ./kernel/scheduler.d \
 ./kernel/syslib.d \
+./kernel/systimer.d \
 ./kernel/task_manage.d \
-./kernel/task_queue.d 
+./kernel/task_queue.d \
+./kernel/task_sync.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
