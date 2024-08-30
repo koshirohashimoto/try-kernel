f8d4c0e131ba0c9f744bb13ba052b65500f107f0
Koshiro Hashimoto (Woven by Toyota <koshiro.hashimoto@woven-planet.global>
2024-08-30 14:01:06 +0900

    complete part4_sec3

---
+++ Debug/kernel/semaphore.d
@@ -0,0 +1,15 @@
+kernel/semaphore.o: ../kernel/semaphore.c \
+ C:\work\TryKernel\include/trykernel.h \
+ C:\work\TryKernel\include/typedef.h C:\work\TryKernel\include/sysdef.h \
+ C:\work\TryKernel\include/syslib.h C:\work\TryKernel\include/config.h \
+ C:\work\TryKernel\include/error.h C:\work\TryKernel\include/knldef.h \
+ C:\work\TryKernel\include/apidef.h C:\work\TryKernel\include/knldef.h
+C:\work\TryKernel\include/trykernel.h:
+C:\work\TryKernel\include/typedef.h:
+C:\work\TryKernel\include/sysdef.h:
+C:\work\TryKernel\include/syslib.h:
+C:\work\TryKernel\include/config.h:
+C:\work\TryKernel\include/error.h:
+C:\work\TryKernel\include/knldef.h:
+C:\work\TryKernel\include/apidef.h:
+C:\work\TryKernel\include/knldef.h:
--- include/config.h
+++ include/config.h
@@ -8,5 +8,6 @@
 #define CNF_MAX_TSKID	32	// Max task number
 #define CNF_MAX_TSKPRI	16	// Max task priority
 #define CNF_MAX_FLGID	8	// Max flag event
+#define CNF_MAX_SEMID	8 	// Max semaphore number
 
 #endif /* CONFIG_H_ */
